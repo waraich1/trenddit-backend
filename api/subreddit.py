094d876b672e6ab711c5586ad43717e839029117
waraich1 <waraich@mcmaster.ca>
2023-01-28 04:14:27 -0500

    Added input for subreddit

--- api/subreddit.py
+++ api/subreddit.py
@@ -14,17 +14,24 @@ subreddit_routes = Blueprint("subreddit", __name__)
 
 @subreddit_routes.route("/subreddit_posts", methods=["GET"])
 async def subreddit_get_posts():
+
+    subreddit_name = request.args.get("subreddit")
+    sort = request.args.get("sort")
+    top = request.args.get("top")
     token = FirebaseC().get_token()
     sub = SubredditF(token)
-    res = await (sub.get_hot_posts("Canada", 100))
+    res = await sub.get_hot_posts(subreddit_name, 100, sort, top)
     response = jsonify(authError=True, data=res)
     return response
 
 
 @subreddit_routes.route("/subreddit_comments", methods=["GET"])
 async def subreddit_get_comments():
+    subreddit_name = request.args.get("subreddit")
+    sort = request.args.get("sort")
+    top = request.args.get("top")
     token = FirebaseC().get_token()
     sub = SubredditF(token)
-    res = await sub.get_hot_comments("pakistan", 50)
+    res = await sub.get_hot_comments(subreddit_name, 50, sort, top)
     response = jsonify(authError=True, data=res)
     return response
